1f26838f919002123f3dde41e7a80d8cd6df6596
lgm527 <laurellmccaffrey@gmail.com>
2019-12-27 17:09:15 -0500

    first

---
+++ App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { StyleSheet, Text, View, Image } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Welcome to TreeTrends!</Text>
+      <Image source={'./assets/treetrends.png'} alt='hi'/>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
