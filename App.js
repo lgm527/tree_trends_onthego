afeede34c664c49837f3a67faad77e20585de99b
lgm527 <laurellmccaffrey@gmail.com>
2019-12-29 15:45:07 -0500

    map set up

--- App.js
+++ App.js
@@ -2,15 +2,28 @@ import React from 'react';
 import { StyleSheet, Text, View, Image, Dimensions } from 'react-native';
 import MapView from 'react-native-maps';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Welcome to</Text>
-      <Image source={require('./assets/treetrends.png')} />
-      <Image source={require('./assets/loraxicon.jpg')} />
-      <MapView style={styles.mapStyle} />
-    </View>
-  );
+export default class App extends React.Component {
+
+  state = {
+    region: {
+      latitude: 40.7085874,
+      longitude: -73.963434,
+      latitudeDelta: 0.01322,
+      longitudeDelta: 0.0821,
+    }
+  }
+
+
+  render(){
+    return (
+      <View style={styles.container}>
+        <Text>Welcome to</Text>
+        <Image source={require('./assets/treetrends.png')} />
+        <Image source={require('./assets/loraxicon.jpg')} />
+        <MapView style={styles.mapStyle} region={this.state.region} />
+      </View>
+    )
+  }
 }
 
 const styles = StyleSheet.create({
