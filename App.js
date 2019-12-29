97216582b260480b9690ff35bbbdb8c336c5685f
lgm527 <laurellmccaffrey@gmail.com>
2019-12-29 15:26:55 -0500

    map added

--- App.js
+++ App.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import { StyleSheet, Text, View, Image } from 'react-native';
+import { StyleSheet, Text, View, Image, Dimensions } from 'react-native';
+import MapView from 'react-native-maps';
 
 export default function App() {
   return (
@@ -7,6 +8,7 @@ export default function App() {
       <Text>Welcome to</Text>
       <Image source={require('./assets/treetrends.png')} />
       <Image source={require('./assets/loraxicon.jpg')} />
+      <MapView style={styles.mapStyle} />
     </View>
   );
 }
@@ -14,8 +16,12 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'rgba(0, 173, 35, .6)',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  mapStyle: {
+    width: Dimensions.get('window').width-20,
+    height: Dimensions.get('window').height/2,
+  }
 });
