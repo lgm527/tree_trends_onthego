0ddc637205bdcb7660d1e266d679d5ea2d6bdd58
lgm527 <laurellmccaffrey@gmail.com>
2019-12-30 14:14:08 -0500

    successful tree fetch

--- App.js
+++ App.js
@@ -10,14 +10,41 @@ export default class App extends React.Component {
       longitude: -73.963434,
       latitudeDelta: 0.01322,
       longitudeDelta: 0.0821,
-    }
+    },
+    trees: [],
+    neighborhood: 'Williamsburg'
+  }
+
+  componentDidMount() {
+    this.treeFetch(this.state.neighborhood);
+  }
+
+  treeFetch(n){
+    fetch(`https://data.cityofnewyork.us/resource/uvpi-gqnh.json?nta_name=${n}&status=Alive&steward=None`,{
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json',
+        'X-App-Token': 'muZjL5D3H5UGU2TUlPOR4IjM6'
+      }
+    })
+    .then(res => res.json())
+    .then(trees => {
+      console.log(trees);
+      // this.setState({
+      //   trees: trees,
+      //   region: {
+      //     latitude: trees[400].latitude,
+      //     longitude: trees[400].longitude
+      //   }
+      // })
+    })
   }
 
 
-  render(){
+  render() {
     return (
       <View style={styles.container}>
-        <Text>Welcome to</Text>
         <Image source={require('./assets/treetrends.png')} />
         <Image source={require('./assets/loraxicon.jpg')} />
         <MapView style={styles.mapStyle} region={this.state.region} />
